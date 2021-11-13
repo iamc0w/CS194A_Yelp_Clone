7bf5c77f7b99bfba37ec30efe563d916b03a6641
Yue Yang <yangyue@DNa1f03c1.SUNet>
2021-11-13 01:37:06 -0800

    search v1

--- app/src/main/java/com/example/yelpclone/MainActivity.kt
+++ app/src/main/java/com/example/yelpclone/MainActivity.kt
@@ -1,8 +1,14 @@
 package com.example.yelpclone
 
+import android.app.Activity
+import android.app.SearchManager
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import android.widget.SearchView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import retrofit2.Call
@@ -13,21 +19,36 @@ import retrofit2.converter.gson.GsonConverterFactory
 private const val TAG = "MainActivity"
 private const val BASE_URL = "https://api.yelp.com/v3/"
 private const val API_KEY = "FO7mfcxxC0kG6BYhNcbSUcIFPMduMEoZwApkZp-XMkWibkOErOTZCAZ0SXPsOOTQ9pCNNIEZH-VqKMYrPUBINamVtB2pNxhwm0JIhT84tdC3i86kGl9gzswKG5KHYXYx"
+private const val DEFAULT_LOCATION = "New York"
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var restaurants: MutableList<YelpRestaurant>
+    private lateinit var adapter: RestaurantsAdapter
+    private lateinit var yelpService: YelpService
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.i(TAG, "onCreate")
         setContentView(R.layout.activity_main)
 
-        val restaurants = mutableListOf<YelpRestaurant>()
-        val adapter = RestaurantsAdapter(this, restaurants)
+        restaurants = mutableListOf<YelpRestaurant>()
+        adapter = RestaurantsAdapter(this, restaurants)
         val rvRestaurants : RecyclerView = findViewById(R.id.rvRestaurants)
         rvRestaurants.adapter = adapter
         rvRestaurants.layoutManager = LinearLayoutManager(this)
-
         val retrofit = Retrofit.Builder().baseUrl(BASE_URL).addConverterFactory(GsonConverterFactory.create()).build()
-        val yelpService = retrofit.create(YelpService::class.java)
-        yelpService.searchRestaurants("Bearer $API_KEY","Avocado Toast", "New York").enqueue(object : Callback<YelpSearchResult> {
+        yelpService = retrofit.create(YelpService::class.java)
+
+        handleIntent(intent)
+    }
+
+    override fun onNewIntent(intent: Intent) {
+        super.onNewIntent(intent)
+        handleIntent(intent)
+    }
+
+    private fun make_yelp_request(searchTerm: String, location: String) {
+        yelpService.searchRestaurants("Bearer $API_KEY",searchTerm, location).enqueue(object : Callback<YelpSearchResult> {
             override fun onResponse(call: Call<YelpSearchResult>, response: Response<YelpSearchResult>) {
                 Log.i(TAG, "onResponse$response")
                 val body = response.body()
@@ -44,4 +65,26 @@ class MainActivity : AppCompatActivity() {
             }
         })
     }
+
+
+    private fun handleIntent(intent: Intent) {
+        if (Intent.ACTION_SEARCH == intent.action) {
+            val query = intent.getStringExtra(SearchManager.QUERY)
+            Log.i(TAG, "handleIntent with query $query")
+            if (query != null) {
+                make_yelp_request(query, DEFAULT_LOCATION)
+            }
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.options_menu, menu)
+
+        val searchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
+        (menu.findItem(R.id.search).actionView as SearchView).apply {
+            setSearchableInfo(searchManager.getSearchableInfo(componentName))
+        }
+        return true
+    }
+
 }
